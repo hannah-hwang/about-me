d7b52bf3b0d8d623aea83325222dea02f75604f1
Hannah Hwang <hannah.hwang1125@gmail.com>
2022-10-24 23:52:37 -0700

    loops....AHDKD

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ let usersName = prompt('What is your name?');
 console.log('users name: ' + usersName);
 alert('Welcome to my website ' + usersName + '! Let\'s get things started with a pop quiz! :)');
 
+let score = 0;
+
 let answer1 = prompt('Was I born in fall?');
 answer1 = answer1.toLowerCase();
 console.log('Fall question response: ' + answer1);
 
 if (answer1 === 'yes' || answer1 === 'y') {
     alert('Correct! I was born in November.');
+    score++;
 } else if (answer1 === 'no' || answer1 === 'n') {
     alert('Wrooong! I was born in November.');
 } else {
@@ -23,6 +26,7 @@ console.log('Pets question response: ' + answer2);
 
 if (answer2 === 'yes' || answer2 === 'y') {
     alert('Yep! I have 1 cat.');
+    score++;
 } else if (answer2 === 'no' || answer2 === 'n') {
     alert('Incorrect! I have 1 cat.');
 } else {
@@ -33,10 +37,11 @@ let answer3 = prompt('Am I trilingual?');
 answer3 = answer3.toLowerCase();
 console.log('Trilingual question response: ' + answer3);
 
-if (answer3 === 'yes' || answer3 === 'y') {
-    alert('Nope! I am actually bilingual.');
-} else if (answer3 === 'no' || answer3 === 'n') {
+if (answer3 === 'no' || answer3 === 'n') {
     alert('Correct! I am actually bilingual.');
+    score++;
+} else if (answer3 === 'yes' || answer3 === 'y') {
+    alert('Nope! I am actually bilingual.');
 } else {
     alert('Please answer with "yes/y" or "no/n".');
 };
@@ -47,6 +52,7 @@ console.log('Cozy games response: ' + answer4);
 
 if (answer4 === 'yes' || answer4 === 'y') {
     alert('Yes! I love playing cozy games :)');
+    score++;
 } else if (answer4 === 'no' || answer4 === 'n') {
     alert('Nooo I love cozy games!');
 } else {
@@ -59,10 +65,39 @@ console.log('Jigsaw puzzle response: ' + answer5);
 
 if (answer5 === 'no' || answer5 === 'n') {
     alert('You\'re right! I do like doing puzzles though!');
+    score++;
 } else if (answer5 === 'yes' || answer5 === 'y') {
-    alert('Good guess! I do enjoy doing puzzles though.');
+    alert('Good guess! No, but I do enjoy doing puzzles though.');
 } else {
     ('Please answer with "yes/y" or "no/n".')
 };
+
+let myNumber = 4;
+let attempts = 4;
+for (let i = 0; i < myNumber; i++) {
+    let answer6 = prompt('Guess a number between 1-10.');
+    console.log('Number response: ' + answer6);
+    if (answer6 === myNumber) {
+        alert('Correct!');
+        score++;
+        break;
+    }
+    else if (answer6 < myNumber) {
+        alert('Nope! You guessed too low.');
+        attempts--;
+    }
+    else if (answer6 > myNumber) {
+        alert('Nope! You guessed too high.');
+        attempts--;
+    }
+}
+
+let favTea = ['oolong tea', 'black tea', 'barley tea', 'green tea']
+for (let i = 0; i < favTea.length; i++) {
+    let answer7 = prompt('Which of the following is my favorite tea: oolong tea, matcha, black tea, barley tea, jasmine tea, green tea, or earl gray tea?');
+    console.log('Favorite tea response:' + answer7)
+}
+
+alert('Your final score is ' + score + ' out of 7!')
 
 alert('Good job ' + usersName + '! Thanks for playing!');
