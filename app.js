c651d1905326831208baa97b6e7bfdb7f7216135
Hannah Hwang <hannah.hwang1125@gmail.com>
2022-10-31 22:39:48 -0700

    changes

--- app.js
+++ app.js
@@ -7,104 +7,104 @@ alert('Welcome to my website ' + usersName + '! Let\'s get things started with a
 
 let score = 0;
 
-function question1(){
-let answer1 = prompt('Was I born in fall?');
-answer1 = answer1.toLowerCase();
-console.log('Fall question response: ' + answer1);
+function question1() {
+    let answer1 = prompt('Was I born in fall?');
+    answer1 = answer1.toLowerCase();
+    console.log('Fall question response: ' + answer1);
 
-if (answer1 === 'yes' || answer1 === 'y') {
-    alert('Correct! I was born in November.');
-    score++;
-} else if (answer1 === 'no' || answer1 === 'n') {
-    alert('Wrooong! I was born in November.');
-} else {
-    alert('Please answer with "yes/y" or "no/n".');
-}
+    if (answer1 === 'yes' || answer1 === 'y') {
+        alert('Correct! I was born in November.');
+        score++;
+    } else if (answer1 === 'no' || answer1 === 'n') {
+        alert('Wrooong! I was born in November.');
+    } else {
+        alert('Please answer with "yes/y" or "no/n".');
+    }
 }
 question1();
-function question2(){
-let answer2 = prompt('Do I have any pets?');
-answer2 = answer2.toLowerCase();
-console.log('Pets question response: ' + answer2);
+function question2() {
+    let answer2 = prompt('Do I have any pets?');
+    answer2 = answer2.toLowerCase();
+    console.log('Pets question response: ' + answer2);
 
-if (answer2 === 'yes' || answer2 === 'y') {
-    alert('Yep! I have 1 cat.');
-    score++;
-} else if (answer2 === 'no' || answer2 === 'n') {
-    alert('Incorrect! I have 1 cat.');
-} else {
-    alert('Please answer with "yes/y" or "no/n".');
-}
+    if (answer2 === 'yes' || answer2 === 'y') {
+        alert('Yep! I have 1 cat.');
+        score++;
+    } else if (answer2 === 'no' || answer2 === 'n') {
+        alert('Incorrect! I have 1 cat.');
+    } else {
+        alert('Please answer with "yes/y" or "no/n".');
+    }
 }
 question2();
 
-function question3(){
-let answer3 = prompt('Am I trilingual?');
-answer3 = answer3.toLowerCase();
-console.log('Trilingual question response: ' + answer3);
+function question3() {
+    let answer3 = prompt('Am I trilingual?');
+    answer3 = answer3.toLowerCase();
+    console.log('Trilingual question response: ' + answer3);
 
-if (answer3 === 'no' || answer3 === 'n') {
-    alert('Correct! I am actually bilingual.');
-    score++;
-} else if (answer3 === 'yes' || answer3 === 'y') {
-    alert('Nope! I am actually bilingual.');
-} else {
-    alert('Please answer with "yes/y" or "no/n".');
-}
+    if (answer3 === 'no' || answer3 === 'n') {
+        alert('Correct! I am actually bilingual.');
+        score++;
+    } else if (answer3 === 'yes' || answer3 === 'y') {
+        alert('Nope! I am actually bilingual.');
+    } else {
+        alert('Please answer with "yes/y" or "no/n".');
+    }
 }
 question3();
 
-function question4(){
-let answer4 = prompt('Do I enjoy playing cozy games?');
-answer4 = answer4.toLowerCase();
-console.log('Cozy games response: ' + answer4);
+function question4() {
+    let answer4 = prompt('Do I enjoy playing cozy games?');
+    answer4 = answer4.toLowerCase();
+    console.log('Cozy games response: ' + answer4);
 
-if (answer4 === 'yes' || answer4 === 'y') {
-    alert('Yes! I love playing cozy games :)');
-    score++;
-} else if (answer4 === 'no' || answer4 === 'n') {
-    alert('Nooo I love cozy games!');
-} else {
-    alert('Please answer with "yes/y" or "no/n".');
-}
+    if (answer4 === 'yes' || answer4 === 'y') {
+        alert('Yes! I love playing cozy games :)');
+        score++;
+    } else if (answer4 === 'no' || answer4 === 'n') {
+        alert('Nooo I love cozy games!');
+    } else {
+        alert('Please answer with "yes/y" or "no/n".');
+    }
 }
 question4();
 
-function question5(){
-let answer5 = prompt('Do I collect jigsaw puzzles?');
-answer5 = answer5.toLowerCase();
-console.log('Jigsaw puzzle response: ' + answer5);
+function question5() {
+    let answer5 = prompt('Do I collect jigsaw puzzles?');
+    answer5 = answer5.toLowerCase();
+    console.log('Jigsaw puzzle response: ' + answer5);
 
-if (answer5 === 'no' || answer5 === 'n') {
-    alert('You\'re right! I do like doing puzzles though!');
-    score++;
-} else if (answer5 === 'yes' || answer5 === 'y') {
-    alert('Good guess! No, but I do enjoy doing puzzles though.');
-} else {
-    ('Please answer with "yes/y" or "no/n".')
-}
+    if (answer5 === 'no' || answer5 === 'n') {
+        alert('You\'re right! I do like doing puzzles though!');
+        score++;
+    } else if (answer5 === 'yes' || answer5 === 'y') {
+        alert('Good guess! No, but I do enjoy doing puzzles though.');
+    } else {
+        ('Please answer with "yes/y" or "no/n".')
+    }
 }
 question5();
 
-function guessNumber(){
-for (let i = 0; i < 4; i++) {
-    let answer6 = prompt('Guess a number between 1-10.');
-    console.log('Number response: ' + answer6);
-    if (answer6 === "6") {
-        alert('Correct!');
-        score++;
-        break;
-    }
-    else if (answer6 < 6) {
-        alert('Nope! You guessed too low.');
-    }
-    else if (answer6 > 6) {
-        alert('Nope! You guessed too high.');
-    }
-    if (i >= 3) {
-        alert('Sorry! The number was 6.')
-    }
-};
+function guessNumber() {
+    for (let i = 0; i < 4; i++) {
+        let answer6 = prompt('Guess a number between 1-10.');
+        console.log('Number response: ' + answer6);
+        if (answer6 === "6") {
+            alert('Correct!');
+            score++;
+            break;
+        }
+        else if (answer6 < 6) {
+            alert('Nope! You guessed too low.');
+        }
+        else if (answer6 > 6) {
+            alert('Nope! You guessed too high.');
+        }
+        if (i >= 3) {
+            alert('Sorry! The number was 6.')
+        }
+    };
 }
 guessNumber();
 
